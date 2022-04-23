f989e3ed5283ec62dd8c1d8c47ad4f840803c6e4
km8oz <ouazmir@gmail.com>
2022-04-23 18:58:19 +0300

    0.0.1

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -3,26 +3,13 @@ import { chdir, cwd } from 'process';
 import path from "path";
 
 import { connect } from '../index.js'
-let clientkey = path.join(cwd(), "/client_keys.ron");
-let serverkey = path.join(cwd(), "/wg_server.ron");
-connect(clientkey, serverkey)
-    // test('connect from native', (t) => {
-    //     try {
-    //         let clientkey = path.join(cwd(), "/client_keys.ron");
-    //         let serverkey = path.join(cwd(), "/wg_server.ron");
-    //         connect(clientkey, serverkey)
-    //         t.pass()
-    //     } catch (error) {
-    //         t.fail(error)
-    //     }
-    // })
-    // setTimeout(() => {
-    //   test('close from native', (t) => {
-    //     try {
-    //         close()
-    //         t.pass()
-    //     } catch (error) {
-    //         t.fail(error)
-    //     }
-    // })
-    // }, 12000)
+test('connect from native', (t) => {
+    try {
+        let clientkey = path.join(cwd(), "/client_keys.ron");
+        let serverkey = path.join(cwd(), "/wg_server.ron");
+        connect(clientkey, serverkey)
+        t.pass()
+    } catch (error) {
+        t.fail(error)
+    }
+})
